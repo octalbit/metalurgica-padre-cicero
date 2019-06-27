edb3a32a38893742321faf615511fc71224b3605
Rafael Souza <rafaelangical2@gmail.com>
2019-06-26 22:42:46 -0300

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,31 @@
+
+#!/bin/sh
+
+TMP_DIR=$(mktemp -d)
+
+echo "\033[1;33m What have you done to the site?\033[0m"
+read update
+
+# Create bundle
+npm run build
+git commit -am "Create new bundle"
+
+# Copy build to temp folder
+cp -r ./build/ $TMP_DIR/build
+BUILD_FILES=`ls ./build`
+
+# Wipe out current site
+git checkout master
+git rm -rf .
+
+# Move new bundle to master
+mv $TMP_DIR/build/** ./ -f
+
+# Commit and push changes
+git commit -m "$update"
+echo "\033[1;31mPushing new site to GitHub repo\n\033[0m"
+git push origin master --force
+
+# Get back to develop
+git checkout develop
+
