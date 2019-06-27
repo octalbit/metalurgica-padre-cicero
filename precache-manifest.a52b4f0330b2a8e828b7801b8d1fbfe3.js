55e3cd5fbd87bca5c77bf30002ced50a57a14bdd
Rafael Souza <rafaelangical2@gmail.com>
2019-06-26 22:55:12 -0300

    Push new site

---
+++ precache-manifest.a52b4f0330b2a8e828b7801b8d1fbfe3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c041bc672fa7f8c8fc47ef14d6ea670",
+    "url": "/static/media/img4.4c041bc6.jpg"
+  },
+  {
+    "revision": "63d5076e7fff49266cc9",
+    "url": "/static/css/main.d4efbdd4.chunk.css"
+  },
+  {
+    "revision": "f422ad34dfc9d5bc2e80",
+    "url": "/static/js/1.f422ad34.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8361b79307809714cba7bf9795514c49",
+    "url": "/static/media/img.8361b793.jpg"
+  },
+  {
+    "revision": "3a9333a95eb2ebbaf69eb63881c46dc1",
+    "url": "/static/media/img2.3a9333a9.png"
+  },
+  {
+    "revision": "f71fd8d16e77812c92631e3756067d0f",
+    "url": "/static/media/img3.f71fd8d1.png"
+  },
+  {
+    "revision": "593b9b68287fcee3b36174b06768b92c",
+    "url": "/static/media/img4.593b9b68.png"
+  },
+  {
+    "revision": "63d5076e7fff49266cc9",
+    "url": "/static/js/main.63d5076e.chunk.js"
+  },
+  {
+    "revision": "885611187b1a82b889e7aae0531eb5ee",
+    "url": "/static/media/img6.88561118.jpg"
+  },
+  {
+    "revision": "d373775078ae8a32ce51310df924ebf5",
+    "url": "/static/media/img5.d3737750.jpg"
+  },
+  {
+    "revision": "386ac1e2340b5ab9e3dd91d39311f359",
+    "url": "/static/media/img8.386ac1e2.jpeg"
+  },
+  {
+    "revision": "fd334ff8d7b80166f6a268fdc93a7f78",
+    "url": "/static/media/img12.fd334ff8.jpeg"
+  },
+  {
+    "revision": "532dfc6fe201e79f78936fba61ab2ee5",
+    "url": "/static/media/bg-about.532dfc6f.png"
+  },
+  {
+    "revision": "82f3ffae3c38ac54b075bb5b388faa57",
+    "url": "/static/media/bg-testemunhas.82f3ffae.png"
+  },
+  {
+    "revision": "3fe8738d9ab34811b44b27a3d4297370",
+    "url": "/static/media/hero-bg.3fe8738d.png"
+  },
+  {
+    "revision": "5831597f15a40ba587283b0dc21d8082",
+    "url": "/index.html"
+  }
+];
